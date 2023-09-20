5a87a52192346061c49bc954b65ceab1979e9ea2
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2023-09-20 12:07:26 +0300

    add login

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
-import Drawer from '../components/Drawer'
+import React, { useState } from 'react'
 
 function SignIn() {
+
+  const [selectedRole, setSelectedRole] = useState('Creator');
+
+  const handleRoleChange = () => {
+    setSelectedRole(e.target.value)
+  }
+
+
   return (
     <div className='pl-12'>
        <section className='bg-transparent'>
@@ -9,11 +16,24 @@ function SignIn() {
         <h2 className='text-2xl tracking-wide text-white/60'>Let's create your account</h2>
        </section>
 
-       <form className='mt-16'>
-        <label className=' text-base text-white'>Enter your email address...</label><br/>
+       <form className='mt-14'>
+        <label className=' text-white text-lg font-normal'>Sign in as:</label><br/>
+        <input type="radio" name="role" value={'Creator'} id="" className='mr-1' /><span className=' text-white mr-6'>Creator</span>
+        <input type="radio" name="role" value={'Editor'} id=""  className='mr-1' /><span className='text-white'>Editor</span>
+        <br/>
+        <div className='mt-7'>
+        <label className=' text-base text-white '>Enter your email address...</label><br/>
         <input type="email" name="email" id="email" className='bg-gray-100 mt-5 py-2 px-5 w-80 rounded-md  ' placeholder='xxx@aaaa.com'  /> <br/>
+        </div>
 
         <button type="submit" className='bg-[#f6bf53] py-3 px-24 mt-16 rounded-full font-semibold text-base'>Send Magic Link</button>
+
+        <p className="text-xs text-gray-500 text-center mt-9 -ml-14">
+          Do not have an account?
+          <button className="underline text-yellow-600 ml-2 font-semibold">
+            Sign up
+          </button>
+        </p>
        </form>
 
        
